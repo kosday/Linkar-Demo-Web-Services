a359c6d4bd086cf1677cabed2372586b87b9011a
kosday <47530763+kosday@users.noreply.github.com>
2020-02-26 08:34:07 +0100

    First commit

---
+++ LINKARDEMOSERVICES/LinkarDemoWCF/Service1.svc.cs
@@ -0,0 +1,20 @@
+﻿using LinkarServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LinkarDemoWCF
+{
+    public class Service1 : IService1
+    {
+        public string GetData(string text, int seconds)
+        {
+            //Call common project
+            return Functions.SubDemoLinkar(text, seconds);
+        }
+    }
+}
